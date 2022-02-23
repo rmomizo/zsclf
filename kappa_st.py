a19c5fa2a7bbef57e1d450f733d1b4e7ee6b8e13
rmomizo <rmomizo@users.noreply.github.com>
2022-02-23 10:32:45 -0500

    Update kappa_st.py

--- kappa_st.py
+++ kappa_st.py
@@ -19,14 +19,19 @@ st.subheader("Calculate the inter-rater agreement between two coders using sklea
 st.subheader('How to use the calculator:')
 st.text("""1. Copy the codes of Coder 1 into the Coder 1 text entry field and hit "Enter." 
 2. Copy the codes for Coder 2 into the Coder 2 text entry field and hit "Enter."
+
+🗒️ Make sure that the coding decisions between Coder 1 and Coder 2 are the same length.
 """)
 
 kappa_button()
 
 st.write("""
 ## References
+Boettger, R. K., & Palmer, L. A. (2010). Quantitative content analysis: Its use in technical communication. IEEE transactions on professional communication, 53(4), 346-357.
 
 Cohen, J. (1960). A coefficient of agreement for nominal scales. Educational and psychological measurement, 20(1), 37-46.
+
+Geisler, C., & Swarts, J. (2019). Coding streams of language: Techniques for the systematic coding of text, talk, and other verbal data. Ft. Collins, CO: WAC Clearinghouse.
 
 Pedregosa, F. and Varoquaux, G. and Gramfort, A. and Michel, V. and Thirion, B. and Grisel, O. and Blondel, M. and Prettenhofer, P.and Weiss, R. and Dubourg, V. and Vanderplas, J. and Passos, A. and Cournapeau, D. and Brucher, M. and Perrot, M. and Duchesnay, E. (2011). Scikit-Learn: Machine Learning in Python. the Journal of machine Learning research, 12, 2825-2830.
 
