f27b05fc0112b104c4ef7477cac3b1c6a543fd0b
rmomizo <rmomizo@users.noreply.github.com>
2022-02-24 16:28:25 -0500

    Update kappa_st.py

--- kappa_st.py
+++ kappa_st.py
@@ -38,10 +38,10 @@ def chi():
     c1 = st.columns(1)
     c2, c3, c4 = st.columns(3)
     
-    c1.metric('p-value: ', str(p_val))
-    c2.metric('degree of freedom: ',"{:e}".format(dof)) 
-    c3.metric('\n chi2 test statistic: ',"{:.5f}".format(chi)) 
-    c4.metric('critical value: ',"{:.5f}".format(crit_val))
+    c1.metric('p-value', str(p_val))
+    c2.metric('degree of freedom',"{:e}".format(dof)) 
+    c3.metric('\n chi2 test statistic',"{:.5f}".format(chi)) 
+    c4.metric('critical value',"{:.5f}".format(crit_val))
     st.write("For an extended discussion of using chi2 tests for homogeneity for qualitative coding, see [Geisler and Swarts (2019)](https://wac.colostate.edu/docs/books/codingstreams/chapter9.pdf)")
 
 def kappa():
